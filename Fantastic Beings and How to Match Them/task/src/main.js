57d910b9f31ded978112084caba6d80454e36ce9
Charymyrat Garryyev <kcharymyrat@gmail.com>
2024-03-29 09:13:47 +0500

    Stage 2 done

--- Fantastic Beings and How to Match Them/task/src/main.js
+++ Fantastic Beings and How to Match Them/task/src/main.js
@@ -1,1 +1,24 @@
 console.log("works")
+
+const mapTable = document.querySelector("#map");
+
+window.renderMap = (rowsCount, colsCount) => {
+    for (let i = 0; i < rowsCount; i++) {
+        const tr = document.createElement("tr")
+        for (let j = 0; j < colsCount; j++) {
+            const td = document.createElement("td");
+            td.classList.add("cell");
+            tr.appendChild(td);
+        }
+        mapTable.appendChild(tr);
+    }
+}
+
+window.clearMap = () => {
+    while (mapTable.firstChild) {
+        mapTable.removeChild(mapTable.firstChild);
+    }
+    console.log(mapTable);
+}
+
+window.renderMap(5, 5)
