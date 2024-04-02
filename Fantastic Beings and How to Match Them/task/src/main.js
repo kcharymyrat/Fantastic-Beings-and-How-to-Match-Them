43a40c5cd501fbd19880ceef36b8a1f58d78b540
Charymyrat Garryyev <kcharymyrat@gmail.com>
2024-04-02 13:38:23 +0500

    Stage 4 is done

--- Fantastic Beings and How to Match Them/task/src/main.js
+++ Fantastic Beings and How to Match Them/task/src/main.js
@@ -2,16 +2,208 @@ console.log("works")
 
 const mapTable = document.querySelector("#map");
 
+const zouwu = "zouwu"
+const swooping = "swooping"
+const salamander = "salamander"
+const puffskein = "puffskein"
+const kelpie = "kelpie"
+const fantasticBeings = [zouwu, swooping, salamander, puffskein,kelpie]
+
+const threeOrMoreNeighborCoords = [];
+let filledFantasticBeingsArr = createRandomFantasticBeingsArr(5, 5, fantasticBeings)
+
+let firstClickedElement = null
+
+registerAllNeighbors()
+console.log("threeOrMoreNeighbors =", threeOrMoreNeighborCoords)
+
+function createRandomFantasticBeingsArr(rowNum, colNum, animalTypes) {
+    const randomMap = []
+    for (let i = 0; i < rowNum; i++) {
+        randomMap.push(randomBeingsRow(colNum, animalTypes))
+    }
+    return randomMap;
+}
+
+function randomBeingsRow(rowLength, animalTypesArr) {
+    let numbers = Array.from(
+        { length: rowLength },
+        (_, index) => {
+            if (index < animalTypesArr.length) {
+                return animalTypesArr[index]
+            } else {
+                // return animalTypesArr[index % animalTypesArr.length]
+                return animalTypesArr[Math.floor(Math.random() * animalTypesArr.length)]
+            }
+        }
+    );
+
+    // Shuffle the array
+    for (let i = numbers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+    }
+    return numbers;
+}
+
+function handleCellClick(event, i, j, td) {
+    console.log("i =", i, ", j=", j, ", td =", td)
+    if (!firstClickedElement) {
+        td.style.backgroundImage = 'url("images/cell-selected-bg.png")';
+        td.style.backgroundSize = "contain";
+        // neighbors
+        firstClickedElement = {"i": i, "j": j, "element": td}
+    } else {
+        const i1 = firstClickedElement.i;
+        const j1 = firstClickedElement.j;
+        const e1 = firstClickedElement.element;
+
+        const isLeft = i === i1 && j === j1 - 1;
+        const isTop = i === i1 - 1 && j === j1;
+        const isRight = i === i1 && j === j1 + 1;
+        const isBottom = i === i1 + 1 && j === j1;
+
+        console.log(isLeft, isTop, isRight, isBottom)
+
+        if (isLeft || isTop || isRight || isBottom) {
+            firstClickedElement = null;
+            td.style.backgroundImage = "none";
+            const tempBeing = filledFantasticBeingsArr[i1][j1];
+            filledFantasticBeingsArr[i1][j1] = filledFantasticBeingsArr[i][j];
+            filledFantasticBeingsArr[i][j] = tempBeing;
+            registerAllNeighbors()
+            deleteSameNeighbors()
+            console.log("threeOrMoreNeighbors =", threeOrMoreNeighborCoords)
+            redrawMap(filledFantasticBeingsArr);
+            console.log(filledFantasticBeingsArr);
+
+            setTimeout(fillDeleted, 100)
+
+            console.log(filledFantasticBeingsArr)
+        }
+    }
+}
+
+function registerHorizontalNeighbors(colIndexStart, colIndexStop, rowIndex) {
+    const i = rowIndex;
+
+    for (let j = colIndexStart; j <= colIndexStop; j++) {
+        const current = filledFantasticBeingsArr[i][j];
+        const neighbors = [{"i": i, "j": j}];
+
+        for (let k = j + 1; k <= colIndexStop; k++) {
+            const next = filledFantasticBeingsArr[i][k];
+            if (current === next) {
+                neighbors.push({"i": i, "j": k})
+            } else {
+                j = k - 1
+                break
+            }
+        }
+
+        if (neighbors.length >= 3) {
+            threeOrMoreNeighborCoords.push(neighbors);
+        }
+    }
+}
+
+
+function registerVerticalNeighbors(rowIndexStart, rowIndexStop, colIndex) {
+    const j = colIndex;
+
+    for (let i = rowIndexStart; i <= rowIndexStop; i++) {
+        const current = filledFantasticBeingsArr[i][j];
+        const neighbors = [{"i": i, "j": j}];
+
+        for (let k = i + 1; k <= rowIndexStop; k++) {
+            const next = filledFantasticBeingsArr[k][j];
+            if (current === next) {
+                neighbors.push({"i": k, "j": j})
+            } else {
+                i = k - 1
+                break
+            }
+        }
+
+        if (neighbors.length >= 3) {
+            threeOrMoreNeighborCoords.push(neighbors);
+        }
+    }
+}
+
+
+
+function registerAllNeighbors() {
+    for (let i = 0; i < filledFantasticBeingsArr.length; i++) {
+        registerHorizontalNeighbors(0, filledFantasticBeingsArr.length - 1, i)
+    }
+    for (let j = 0; j < filledFantasticBeingsArr[0].length; j++) {
+        registerVerticalNeighbors(0, filledFantasticBeingsArr[j].length - 1, j)
+    }
+}
+
+function deleteSameNeighbors() {
+    if (threeOrMoreNeighborCoords.length < 1) {
+        return false
+    }
+    let isDeleted = false
+    for (const coords of threeOrMoreNeighborCoords) {
+        for (const coord of coords) {
+            filledFantasticBeingsArr[coord.i][coord.j] = ""
+            isDeleted = true
+        }
+    }
+    threeOrMoreNeighborCoords.length = 0
+    return isDeleted
+}
+
+
+function fillDeleted() {
+    for (let i = 0; i < filledFantasticBeingsArr.length; i++) {
+        for (let j = 0; j < filledFantasticBeingsArr[i].length; j++) {
+            if (!filledFantasticBeingsArr[i][j]){
+                filledFantasticBeingsArr[i][j] = window.generateRandomBeingName()
+            }
+        }
+    }
+
+    redrawMap(filledFantasticBeingsArr)
+    registerAllNeighbors()
+    const isDeleted = deleteSameNeighbors()
+    if (isDeleted) {
+        fillDeleted()
+    }
+}
+
 window.renderMap = (rowsCount, colsCount) => {
     for (let i = 0; i < rowsCount; i++) {
         const tr = document.createElement("tr")
+        tr.classList.add("row");
         for (let j = 0; j < colsCount; j++) {
             const td = document.createElement("td");
             td.classList.add("cell");
+            td.addEventListener("click", (event) => {
+                handleCellClick(event, i, j, td);
+            })
+
+            if (filledFantasticBeingsArr[i][j]) {
+                const img = document.createElement("img");
+                img.src = `images/${filledFantasticBeingsArr[i][j]}.png`;
+                img.dataset.coords = `x${j}_y${i}`;
+                img.style.width = 100 + 'px';
+                img.style.height = 100 + 'px'
+                td.dataset.being = filledFantasticBeingsArr[i][j];
+                td.appendChild(img)
+            } else {
+                td.dataset.being = "";
+                td.textContent = "";
+            }
+
             tr.appendChild(td);
         }
         mapTable.appendChild(tr);
     }
+    console.log(mapTable);
 }
 
 window.clearMap = () => {
@@ -21,4 +213,33 @@ window.clearMap = () => {
     console.log(mapTable);
 }
 
+
+window.redrawMap = (arr) => {
+    if (!Array.isArray(arr) || arr.length < 3 || arr.find(el => !Array.isArray(el) || el.length < 3))
+        return false;
+
+    clearMap();
+
+    filledFantasticBeingsArr = arr
+
+    renderMap(arr.length, arr[0].length);
+
+    return true
+}
+
+window.generateRandomBeingName = () => {
+    return fantasticBeings[Math.floor(Math.random() * fantasticBeings.length)]
+}
+
 window.renderMap(5, 5)
+// window.redrawMap([
+//     ['kelpie', 'puffskein', 'puffskein'],
+//     ['swooping', 'zouwu', 'puffskein'],
+//     ['kelpie', 'puffskein', 'zouwu']
+// ]);
+
+
+
+
+
+
--- Fantastic Beings and How to Match Them/task/test/test.js
+++ Fantastic Beings and How to Match Them/task/test/test.js
@@ -2,12 +2,20 @@ import path from 'path';
 const pagePath = path.join(import.meta.url, '../../src/index.html');
 import {StageTest, correct, wrong} from 'hs-test-web';
 
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
 class Test extends StageTest {
 
     page = this.getPage(pagePath)
 
     tests = [
-        // Test#1 - check existence of map element
+        //Test#1 - check existence of map element
         this.page.execute(() => {
             this.map = document.getElementById('map');
 
@@ -15,15 +23,7 @@ class Test extends StageTest {
                 correct() :
                 wrong(`You need to create a table with the ID "map"`)
         }),
-        //Test#2 - check count of tds
-        this.node.execute(async () => {
-            const tds = await this.page.findAllBySelector('td');
-
-            return tds.length === 25 ?
-                correct() :
-                wrong(`The map must be 5x5.`);
-        }),
-        //Test#3 - check that map set class cell to the cells
+        //Test#2 - check that map set class cell to the cells
         this.node.execute(async () => {
             const cells = await this.page.findAllBySelector('.cell');
 
@@ -31,38 +31,128 @@ class Test extends StageTest {
                 correct() :
                 wrong(`Each cell of the map must have a 'cell' class.`);
         }),
-        //Test#4 - check rows count
+        //Test#3 - check render of beings function work
         this.node.execute(async () => {
-            const cells = await this.page.findAllBySelector('tr');
+            this.imgs = await this.page.findAllBySelector('img[data-coords]');
+            this.cells = await this.page.findAllBySelector('.cell[data-being]');
 
-            return cells.length === 5 ?
+            return this.imgs.length === 25 && this.cells.length === 25  ?
                 correct() :
-                wrong(`The map must have 5 rows in the table.`);
+                wrong(`Beings rendering method must fill all empty cells of the map. Now you have ${this.cells.length} cells and ${this.imgs.length} beings.`);
         }),
-        //Test#5 - check clearMap function
+        //Test#4 - check .cell[data-being] property
+        this.page.execute(() => {
+            let beings = ['zouwu', 'swooping', 'salamander', 'puffskein', 'kelpie'];
+            let cellObjects = document.getElementsByClassName('cell');
+            for(let c of cellObjects) {
+                if(!beings.includes(c.dataset.being)) {
+                    return wrong(`Each cell must have a dataset.being property, the value of which must be equal to the name of a random creature from a list of 5 possible creatures.
+                    We see that the property of one of the cells is equal to the value: ${c.dataset.being}`);
+                }
+            }
+            return correct();
+        }),
+        //Test#5 - check img[data-coords] property
+        this.page.execute(() => {
+            let imgObjs = document.querySelectorAll('img[data-coords]');
+            return imgObjs[5].dataset.coords === 'x0_y1' ?
+                correct() :
+                wrong(`Img objects inside the table have an invalid dataset.coords property.`)
+        }),
+        //Test#6 - check background of selected being
+        this.node.execute(async () => {
+            const being = await this.page.findBySelector('img[data-coords=x0_y0]');
+            await being.hover();
+
+            const cell = await this.page.findBySelector('.cell');
+            let style = await cell.getComputedStyles();
+
+            return style.backgroundImage.includes('cell-hover-bg')  ?
+                correct() :
+                wrong(`The clicked cell must have a background image.`);
+        }),
+        //Test#7 - check click on non-neighboring cells
+        this.node.execute(async () => {
+            await this.page.refresh();
+            sleep(500);
+            const being1 = await this.page.findBySelector('img[data-coords=x0_y0]');
+            await being1.click();
+            const being2 = await this.page.findBySelector('img[data-coords=x2_y0]');
+            await being2.click();
+
+            this.cells = await this.page.findAllBySelector('.cell');
+            let style1 = await this.cells[0].getComputedStyles();
+            let style2 = await this.cells[2].getComputedStyles();
+            await this.page.refresh();
+            sleep(500);
+            return style1.backgroundImage.includes('cell-selected-bg') &&
+            style2.backgroundImage === 'none'  ?
+                correct() :
+                wrong(`When you click on one cell and the second click on another, non-adjacent cell, nothing should happen.`);
+        }),
+        //Test#8 - check clearMap function
         this.page.execute(() => {
             if (window.clearMap instanceof Function) {
                 window.clearMap();
             } else {
                 return wrong(`Implement the window.clearMap() function, please.`)
             }
-
             this.cells = document.getElementsByClassName('cell');
             return this.cells.length === 0 ?
                 correct() :
                 wrong(`Check your window.clearMap() function, now after it works, not all map cells are cleared.`)
         }),
-        //Test#6 - check renderMap function
+        //Test#9 - check renderMap function
         this.page.execute(() => {
             if (window.renderMap instanceof Function) {
-                window.renderMap(3, 3);
+                window.renderMap(5, 5);
             } else {
                 return wrong(`Implement the window.renderMap() function, please.`)
             }
             this.cells = document.getElementsByClassName('cell');
-            return this.cells.length === 9 ?
+            return this.cells.length === 25 ?
                 correct() :
-                wrong(`Check your window.renderMap() function. When trying to draw a 3 by 3 map, it draws a map consisting of ${this.cells.length} cells.`)
+                wrong(`Check your window.renderMap() function. When trying to draw a 5 by 5 map, it draws a map consisting of ${this.cells.length} cells.`)
+        }),
+        //Test#10 - check window.redrawMap
+        this.page.execute(() => {
+            window.clearMap();
+            window.renderMap(3, 3);
+            window.redrawMap([
+                ['kelpie', 'puffskein', 'puffskein'],
+                ['zouwu', 'zouwu', 'puffskein'],
+                ['kelpie', 'puffskein', 'zouwu']
+            ]);
+            let cellObjects = document.getElementsByClassName('cell');
+
+            window.generateRandomBeingName = function() {
+                if (!window.generateNameFlag) window.generateNameFlag = 1;
+                window.generateNameFlag++;
+                let being = generateNameFlag % 2 === 0 ? 'puffskein' : 'zouwu';
+                return being;
+            }
+            return cellObjects[5].dataset.being === 'puffskein' && cellObjects[8].dataset.being === 'zouwu' ?
+                correct() :
+                wrong(`Check the window.redrawMap method - at the moment it does not add creatures to the positions specified in the array.`)
+        }),
+        //Test#11 - check click on neighboring cells
+        this.node.execute(async () => {
+
+            const being1 = await this.page.findBySelector('img[data-coords=x1_y2]');
+            await being1.click();
+            const being2 = await this.page.findBySelector('img[data-coords=x2_y2]');
+            await being2.click();
+            sleep(1500);
+
+            this.cells = await this.page.findAllBySelector('.cell[data-being]');
+            const neededCells = ['kelpie', 'puffskein', 'puffskein', 'zouwu', 'puffskein', 'zouwu', 'kelpie', 'zouwu', 'puffskein'];
+            for (let i = 0; i < this.cells.length; i++) {
+                if (await this.cells[i].getAttribute('data-being') !== neededCells[i]) {
+                    return wrong(`After clicking on adjacent elements, the resulting sequences of identical creatures should be removed.`);
+                }
+            }
+
+            return correct();
         }),
     ]
 
